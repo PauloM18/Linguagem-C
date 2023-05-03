6bb00c4b58548dd30562343f9ac8f6bb6abecb9f
Paulo Henrique <111320952+PauloM18@users.noreply.github.com>
2023-05-03 11:11:32 -0300

    atualizar

---
+++ Ponteiro/ende_mem11.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <locale.h>
+void copiarString(char *copiarDaqui, char *colarAqui){
+    while(*copiarDaqui != '\0'){
+        *colarAqui = *copiarDaqui;
+        ++copiarDaqui;
+        ++colarAqui;
+    }
+    *colarAqui = '\0';
+};
+
+int main(void){
+    setlocale(LC_ALL,"portuguese");
+    void copiarString(char *copiarDaqui, char *colarAqui);
+
+    char string1[] = "Pão com mortadela";
+    char string2[20];
+
+    copiarString(string1, string2);
+    printf("%s\n", string2);
+    getchar();
+    return 0;
+}
