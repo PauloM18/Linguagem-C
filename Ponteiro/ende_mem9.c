3b68a368353cb3eb92c3f28ed1a4968f65bc6be8
Paulo Henrique <111320952+PauloM18@users.noreply.github.com>
2023-05-03 10:55:30 -0300

    atualizar

--- Ponteiro/ende_mem9.c
+++ Ponteiro/ende_mem9.c
@@ -3,5 +3,13 @@
 int main(void){
 
     int vetor[3] = {1,2,3};
-    
+    // int *ponteiro = &vetor[2];
+    int *ponteiro = vetor;
+    //alterar o valor do ventor no indice 1
+    *(ponteiro + 1)  = 10;
+    printf("%d", vetor[1]);
+
+    getchar();
+
+    return 0;
 }
